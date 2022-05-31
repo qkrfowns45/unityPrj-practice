8630d72020cf9e1edf0618cb83f4fab542b0dad3
박래준 <qkrfowns45@naver.com>
2022-05-31 19:38:04 +0900

    7번째 커밋 키보드 이동 완료

--- Assets/RealMove.cs
+++ Assets/RealMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RealMove : MonoBehaviour
 {
+    void Awake() {
+        Application.targetFrameRate = 30;
+    }
     void Start()
     {
     }
@@ -11,7 +14,7 @@ public class RealMove : MonoBehaviour
     void Update(){
         //오브젝트 변수 transform을 항상 가지고 있음
         //3차원의 벡터값을 뜻하는 vector3
-        Vector3 vec = new Vector3(0,0.1f,0); 
+        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0); 
         transform.Translate(vec);
     }
 }
