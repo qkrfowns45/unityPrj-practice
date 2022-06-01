cd0019e5282389a4844463632c1d3a8b1d9eb425
박래준 <qkrfowns45@naver.com>
2022-06-01 18:59:45 +0900

    8번째 커밋 몰표로 이동시키기

---
+++ Assets/targettomove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targettomove : MonoBehaviour
+{
+    Vector3 target = new Vector3(3,0.85f,0);
+
+    void Awake() {
+        Application.targetFrameRate = 30;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //현재위치, 목표, 속도로 구성된 함수
+        //transform.position = Vector3.MoveTowards(transform.position,target,1f);
+
+        //2.SmoothDamp
+        // Vector3 velo = Vector3.zero;
+        // transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.1f);
+
+        //3.Lerp
+        //transform.position = Vector3.Lerp(transform.position, target, 0.05f);
+
+        //4.Slefp-구면 선형보간 호를 그리며 이동
+        transform.position = Vector3.Slerp(transform.position, target, 0.05f);
+    }
+}
